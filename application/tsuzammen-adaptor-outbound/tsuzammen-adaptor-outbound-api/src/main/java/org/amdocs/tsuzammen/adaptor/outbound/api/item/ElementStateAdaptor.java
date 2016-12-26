46ff87c056a5525b2c284bfe913e4067207bc419
TALIG <talig@amdocs.com>
2016-12-26 13:44:25 +0200

    add missing class

---
+++ application/tsuzammen-adaptor-outbound/tsuzammen-adaptor-outbound-api/src/main/java/org/amdocs/tsuzammen/adaptor/outbound/api/item/ElementStateAdaptor.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2016 Amdocs Software Systems Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.amdocs.tsuzammen.adaptor.outbound.api.item;
+
+import org.amdocs.tsuzammen.commons.datatypes.Id;
+import org.amdocs.tsuzammen.commons.datatypes.SessionContext;
+import org.amdocs.tsuzammen.commons.datatypes.impl.item.CoreEntity;
+import org.amdocs.tsuzammen.commons.datatypes.item.ElementContext;
+
+import java.net.URI;
+
+public interface ElementStateAdaptor {
+
+  URI getNamespace(SessionContext context, ElementContext elementContext, Id elementId);
+
+  boolean isExist(SessionContext context, ElementContext elementContext, Id elementId);
+
+  CoreEntity get(SessionContext context, ElementContext elementContext, Id elementId);
+
+  void create(SessionContext context, ElementContext elementContext, CoreEntity element);
+
+  void save(SessionContext context, ElementContext elementContext, CoreEntity element);
+}
